82da045b2e682fed0a60b54f64308ff8030febd2
Hussein <hussein.elhewehii@independesk.com>
2022-12-11 16:55:26 +0100

    envelope addon

--- index.php
+++ index.php
@@ -4,11 +4,15 @@
 include "./Services/CurlService.php";
 
 $url = "https://appmsds-6aa0.kxcdn.com/content.php?lang=de&json=1&search_text=berlin&currencyiso=EUR";
+$url2 = "https://www.mypostcard.com/mobile/product_prices.php?json=1&type=get_postcard_products&currencyiso=EUR&store_id=";
 $service = new CurlService();
 $output = $service->get($url);
 $outputArray = json_decode($output, true);
 $content = array_slice($outputArray['content'], 0, 25);
-// echo "<pre>";print_r($content[0]);die;
+// echo "<pre>";print_r($content[0]);
+// echo "<pre>";print_r(json_decode($service->get($url2.$content[0]['id']), true)['products'][0]['product_options']['Envelope']);die;
+
+$backgrounds = ["red", "blue", "green"];
 ?>
 
 
@@ -16,6 +20,7 @@ $content = array_slice($outputArray['content'], 0, 25);
 <html lang="en">
 
 <?php include("./partials/headers.php"); ?>
+<?php include("./partials/styles.php"); ?>
 
 <body>
 <div class="container">
@@ -26,6 +31,7 @@ $content = array_slice($outputArray['content'], 0, 25);
         <th>Thumbnail</th>
         <th>Title</th>
         <th>Price (€)</th>
+        <th>4th row</th>
       </tr>
     </thead>
     <tbody>
@@ -33,9 +39,20 @@ $content = array_slice($outputArray['content'], 0, 25);
         foreach ($content as $item) {
         ?>
           <tr>  
-            <td><img style="width: 200px;" src="<?php echo $item['thumb_url'];?>" alt=""></td>
+            <td><img style="width: 200px; height:150px;" src="<?php echo $item['thumb_url'];?>" alt=""></td>
             <td><?php echo $item['title'];?></td>
-            <td><?php echo $item['price'];?></td>
+            <td>
+              <?php 
+                echo $item['price'];
+                if($item['is_greeting_card']){
+                  $products = json_decode($service->get($url2.$item['id']), true);
+                  // echo "<pre>";print_r($products);die;
+                  $envelope = $products['products'][0]['product_options']['Envelope'];
+                  echo ", envelope addon: ".$envelope['price'];
+                }
+              ?>
+            </td>
+            <td class="background <?php echo $backgrounds[array_rand($backgrounds)]?>"></td>
           </tr>
         <?php
         }
---
+++ partials/styles.php
@@ -0,0 +1,15 @@
+<style>
+    .red{
+        background-color: #d1633f;
+    }
+    .blue{
+        background-color: #5761e6;
+    }
+    .green{
+        background-color: #79e34b;
+    }
+
+    .background{
+        
+    }
+</style>
